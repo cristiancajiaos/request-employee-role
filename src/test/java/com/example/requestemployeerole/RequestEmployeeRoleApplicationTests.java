1efacc46ca300ee5dfc8288c137a08584f361920
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-08-07 10:31:42 -0400

    Primer commit

---
+++ src/test/java/com/example/requestemployeerole/RequestEmployeeRoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.requestemployeerole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestEmployeeRoleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
